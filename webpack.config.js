23c0cb508f2c212a0cf6e884cfc44a2af23de95d
vmwavie <vmwavie@protonmail.com>
2024-10-16 12:53:06 -0300

    [add] webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: {
+    mylib: path.resolve(__dirname, "src/index.ts"),
+  },
+  output: {
+    chunkFilename: "[name].js",
+    filename: "[name].js",
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        loader: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
+    fallback: { crypto: false },
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
